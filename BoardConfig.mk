96cf6a978a7aa9fe46783dcfb4b66025a6171c40
Quarx2k <agent00791@gmail.com>
2012-08-04 21:28:38 +0700

    initial config of JB

---
+++ BoardConfig.mk
@@ -0,0 +1,108 @@
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+TARGET_SPECIFIC_HEADER_PATH := device/fly/iq285/include
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := k4
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8660
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+# Architecture
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+# Flags
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60 -DQCOM_HARDWARE -DNO_QCOM_MVS
+
+# Scorpion optimizations
+TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
+TARGET_USE_SCORPION_PLD_SET := true
+TARGET_SCORPION_BIONIC_PLDOFFS := 6
+TARGET_SCORPION_BIONIC_PLDSIZE := 128
+
+# Recovery
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+TARGET_RECOVERY_UI_LIB := librecovery_ui_k4
+BOARD_CUSTOM_GRAPHICS:= ../../../device/fly/iq285/graphics.c
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776192
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 838859776
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1252770816
+BOARD_FLASH_BLOCK_SIZE := 262144
+BOARD_VOLD_MAX_PARTITIONS := 36
+
+
+# Wifi related defines
+WPA_SUPPLICANT_VERSION           := VER_0_6_X
+BOARD_WPA_SUPPLICANT_DRIVER      := WEXT
+BOARD_HOSTAPD_DRIVER             := WEXT
+BOARD_WLAN_DEVICE                := bcm4329
+#BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
+WIFI_DRIVER_FW_PATH_STA          := "/system/etc/firmware/fw_bcm4329.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/system/etc/firmware/fw_bcm4329_hotspot.bin"
+#WIFI_DRIVER_FW_PATH_P2P         := "/vendor/firmware/fw_bcmdhd_p2p.bin"
+WIFI_DRIVER_MODULE_NAME          := eth0
+WIFI_DRIVER_MODULE_ARG           := "firmware_path=/system/etc/firmware/fw_bcm4329.bin"
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/bcm4329.ko"
+
+# Audio
+COMMON_GLOBAL_CFLAGS += -DWITH_QCOM_LPA
+TARGET_USES_QCOM_LPA := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := k4
+
+# Graphics
+COMMON_GLOBAL_CFLAGS += -DQCOM_ROTATOR_KERNEL_FORMATS
+USE_OPENGL_RENDERER := true
+TARGET_HAVE_BYPASS := false
+TARGET_USES_C2D_COMPOSITION := true
+TARGET_USES_OVERLAY := false
+TARGET_QCOM_HDMI_OUT := false
+BOARD_EGL_CFG := device/fly/iq285/egl.cfg
+BOARD_HAVE_HTC_FFC := true
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_USES_ION := false
+
+# Webkit
+ENABLE_WEBGL := true
+TARGET_FORCE_CPU_UPLOAD := true
+
+# Kernel
+TARGET_PREBUILT_KERNEL := device/fly/iq285/kernel
+
+BOARD_USE_LEGACY_TOUCHSCREEN := true
+
---
+++ k4-blobs.mk
@@ -0,0 +1,102 @@
+# Copyright (C) 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+device_path = device/fly/iq285
+
+DEVICE_PREBUILT := ${device_path}/prebuilt
+
+# EGL Stuff
+PRODUCT_COPY_FILES += \
+	$(DEVICE_PREBUILT)/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
+	$(DEVICE_PREBUILT)/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+	$(DEVICE_PREBUILT)/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+	$(DEVICE_PREBUILT)/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
+        $(DEVICE_PREBUILT)/lib/libgsl.so:/system/lib/libgsl.so \
+# Etc
+PRODUCT_COPY_FILES += \
+	$(DEVICE_PREBUILT)/etc/firmware/fw_bcm4329.bin:system/etc/firmware/fw_bcm4329.bin \
+	$(DEVICE_PREBUILT)/etc/firmware/fw_bcm4329_hotspot.bin:system/etc/firmware/fw_bcm4329_hotspot.bin \
+	$(DEVICE_PREBUILT)/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
+	$(DEVICE_PREBUILT)/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
+	$(DEVICE_PREBUILT)/etc/firmware/vidc_1080p.fw:system/etc/firmware/vidc_1080p.fw \
+	$(DEVICE_PREBUILT)/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
+	$(DEVICE_PREBUILT)/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+	$(DEVICE_PREBUILT)/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+	$(DEVICE_PREBUILT)/etc/thermald.conf:system/etc/thermald.conf \
+	$(DEVICE_PREBUILT)/etc/init.qcom.modem_links.sh:system/etc/init.qcom.modem_links.sh \
+
+# Modules
+PRODUCT_COPY_FILES += $(DEVICE_PREBUILT)/lib/modules/bcm4329.ko:/system/lib/modules/bcm4329.ko
+
+# Bins
+
+PRODUCT_COPY_FILES += \
+	$(DEVICE_PREBUILT)/bin/BCM4329B1.hcd:/system/bin/BCM4329B1.hcd \
+	$(DEVICE_PREBUILT)/bin/port-bridge:/system/bin/port-bridge \
+	$(DEVICE_PREBUILT)/bin/qmuxd:/system/bin/qmuxd \
+	$(DEVICE_PREBUILT)/bin/thermald:/system/bin/thermald \
+	$(DEVICE_PREBUILT)/bin/time_daemon:/system/bin/time_daemon \
+	$(DEVICE_PREBUILT)/bin/rmt_storage:/system/bin/rmt_storage \
+	$(DEVICE_PREBUILT)/bin/battery_charging:/system/bin/battery_charging \
+	$(DEVICE_PREBUILT)/bin/netmgrd:/system/bin/netmgrd \
+	$(DEVICE_PREBUILT)/bin/mpdecision:/system/bin/mpdecision \
+
+
+#Libs
+PRODUCT_COPY_FILES += \
+	$(DEVICE_PREBUILT)/lib/libaudioalsa.so:/obj/lib/libaudioalsa.so \
+	$(DEVICE_PREBUILT)/lib/libaudioalsa.so:/system/lib/libaudioalsa.so \
+	$(DEVICE_PREBUILT)/lib/libacdbloader.so:/system/lib/libacdbloader.so \
+	$(DEVICE_PREBUILT)/lib/libacdbloader.so:/obj/lib/libacdbloader.so \
+	$(DEVICE_PREBUILT)/lib/libacdbmapper.so:/system/lib/libacdbmapper.so \
+	$(DEVICE_PREBUILT)/lib/libacdbmapper.so:/obj/lib/libacdbmapper.so \
+	$(DEVICE_PREBUILT)/lib/libcamera.so:/obj/lib/libcamera.so \
+	$(DEVICE_PREBUILT)/lib/libcamera.so:/system/lib/libcamera.so \
+	$(DEVICE_PREBUILT)/lib/liboemcamera.so:/system/lib/liboemcamera.so \
+	$(DEVICE_PREBUILT)/lib/libC2D2.so:/system/lib/libC2D2.so \
+	$(DEVICE_PREBUILT)/lib/libdiag.so:/system/lib/libdiag.so \
+	$(DEVICE_PREBUILT)/lib/libOpenVG.so:/system/lib/libOpenVG.so \
+	$(DEVICE_PREBUILT)/lib/libril-qc-1.so:/system/lib/libril-qc-1.so \
+	$(DEVICE_PREBUILT)/lib/libril-qc-qmi-1.so:/system/lib/llibril-qc-qmi-1.so \
+	$(DEVICE_PREBUILT)/lib/libril-qcril-hook-oem.so:/system/lib/libril-qcril-hook-oem.so \
+	$(DEVICE_PREBUILT)/lib/libmmjpeg.so:/system/lib/libmmjpeg.so \
+	$(DEVICE_PREBUILT)/lib/libSktEngine.so:/system/lib/libSktEngine.so \
+	$(DEVICE_PREBUILT)/lib/libmmipl.so:/system/lib/libmmipl.so \
+	$(DEVICE_PREBUILT)/lib/libgemini.so:/system/lib/libgemini.so \
+	$(DEVICE_PREBUILT)/lib/liboncrpc.so:/system/lib/liboncrpc.so \
+	$(DEVICE_PREBUILT)/lib/libdsm.so:/system/lib/libdsm.so \
+	$(DEVICE_PREBUILT)/lib/libqueue.so:/system/lib/libqueue.so \
+	$(DEVICE_PREBUILT)/lib/libauth.so:/system/lib/libauth.so \
+	$(DEVICE_PREBUILT)/lib/libcm.so:/system/lib/libcm.so \
+	$(DEVICE_PREBUILT)/lib/libmmgsdilib.so:/system/lib/libmmgsdilib.so \
+	$(DEVICE_PREBUILT)/lib/libgsdi_exp.so:/system/lib/libgsdi_exp.so \
+	$(DEVICE_PREBUILT)/lib/libgstk_exp.so:/system/lib/libgstk_exp.so \
+	$(DEVICE_PREBUILT)/lib/libnv.so:/system/lib/libnv.so \
+	$(DEVICE_PREBUILT)/lib/libpbmlib.so:/system/lib/libpbmlib.so \
+	$(DEVICE_PREBUILT)/lib/libwms.so:/system/lib/libwms.so \
+	$(DEVICE_PREBUILT)/lib/libwmsts.so:/system/lib/libwmsts.so \
+	$(DEVICE_PREBUILT)/lib/libqmi.so:/system/lib/libqmi.so \
+	$(DEVICE_PREBUILT)/lib/libdsutils.so:/system/lib/libdsutils.so \
+	$(DEVICE_PREBUILT)/lib/libidl.so:/system/lib/libidl.so \
+	$(DEVICE_PREBUILT)/lib/libdsi_netctrl.so:/system/lib/libdsi_netctrl.so \
+	$(DEVICE_PREBUILT)/lib/libnetmgr.so:/system/lib/libnetmgr.so \
+	$(DEVICE_PREBUILT)/lib/libqdp.so:/system/lib/libqdp.so \
+	$(DEVICE_PREBUILT)/lib/libril.so:/system/lib/libril.so \
+	$(DEVICE_PREBUILT)/lib/libdsucsd.so:/system/lib/libdsucsd.so \
+	$(DEVICE_PREBUILT)/lib/libskts_vt.so:/system/lib/libskts_vt.so \
+	$(DEVICE_PREBUILT)/lib/libthermal_mitigation.so:/system/lib/libthermal_mitigation.so \
+	$(DEVICE_PREBUILT)/lib/libtime_remote_atom.so:/system/lib/libtime_remote_atom.so \
+
+
+#end of k4-blobs.mk
